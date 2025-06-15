7be835f88d4ad61a8ee2d73aadc87e2018eb6b3e
tavinho <octavior.barassa@hotmail.com>
2025-06-15 20:34:17 -0300

    fix: changed annotation NotNull to NotBlank in EventDTO validations

--- src/main/java/com/ifsp/tavinho/smt_backend/domain/dtos/input/entities/EventDTO.java
+++ src/main/java/com/ifsp/tavinho/smt_backend/domain/dtos/input/entities/EventDTO.java
@@ -1,13 +1,12 @@
 package com.ifsp.tavinho.smt_backend.domain.dtos.input.entities;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record EventDTO(
     @NotBlank(message = "Description is required") String description, 
     @NotBlank(message = "Weekday is required") String weekday, 
-    @NotNull(message = "Start time is required") String startTime, 
-    @NotNull(message = "End time is required") String endTime, 
+    @NotBlank(message = "Start time is required") String startTime, 
+    @NotBlank(message = "End time is required") String endTime, 
     @NotBlank(message = "Classroom ID is required") String classroomId, 
     @NotBlank(message = "Professor ID is required") String professorId, 
     @NotBlank(message = "Course ID is required") String courseId, 
