4e32e333aee5c1e36784472dae80f28acaafaaf3
tavinho <octavior.barassa@hotmail.com>
2025-06-21 00:18:32 -0300

    fix: changed NotBlank annotation from weekday property to NotNull

--- src/main/java/com/ifsp/tavinho/smt_backend/domain/dtos/input/entities/EventDTO.java
+++ src/main/java/com/ifsp/tavinho/smt_backend/domain/dtos/input/entities/EventDTO.java
@@ -3,10 +3,11 @@ package com.ifsp.tavinho.smt_backend.domain.dtos.input.entities;
 import com.ifsp.tavinho.smt_backend.domain.enums.Weekday;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record EventDTO(
     @NotBlank(message = "Description is required") String description, 
-    @NotBlank(message = "Weekday is required") Weekday weekday, 
+    @NotNull(message = "Weekday is required") Weekday weekday, 
     @NotBlank(message = "Start time is required") String startTime, 
     @NotBlank(message = "End time is required") String endTime, 
     @NotBlank(message = "Classroom ID is required") String classroomId, 
